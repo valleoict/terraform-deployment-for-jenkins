813ed598ce6e0250ae62f72b54e277783a5dc99b
valleoict <145077524+valleoict@users.noreply.github.com>
2023-12-02 22:30:10 -0800

    Update keyname.tf

--- keyname.tf
+++ keyname.tf
@@ -1,5 +1,5 @@
 # Defining Key Name for connection
 variable "keyname" {
-  default     = "terraform-project" 
-  description = "jekines_key"
+  default     = "jekines_key" 
+  description = "Name of AWS key pair"
 }
